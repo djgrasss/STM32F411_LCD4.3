4850265d39a20b2b8098a2273fceb6396f2b73f2
Rodrigo Torres <rodrigo.torres@silabs.com>
2016-10-21 15:58:00 -0500

    Adds working sources and Scontruct script

--- src/main.c
+++ src/main.c
@@ -1,233 +1,203 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx_hal.h"
+#include "gpio.h"
 
-#include <avr/io.h>
-#include <avr/power.h>
-// #include <stdio.h>
-#include <util/delay.h>
-#include <compat/deprecated.h>
+/* USER CODE BEGIN Includes */
 
-#include "LCD.h"
-#include "Touch.h"
+/* USER CODE END Includes */
 
-const char text[] PROGMEM = "National Instruments";
-const char text2[] PROGMEM = "rodrigo@volium.com";
+/* Private variables ---------------------------------------------------------*/
 
-// Create a stream to direct stdio.h calls to the right function (putChar)
-static FILE lcd_str = FDEV_SETUP_STREAM (putChar, NULL, _FDEV_SETUP_WRITE);
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
 
-uint16_t touch_x, touch_y;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void Error_Handler(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
 
 int main(void)
 {
-   // This will divide the Main Clock by 2
-   // clock_prescale_set(clock_div_2);
 
-   // Set up LED on board and toggle twice
-   sbi(DDRE,6);
+  /* USER CODE BEGIN 1 */
 
-   sbi(PORTE,6);
-   _delay_ms(200);
-   cbi(PORTE,6);
-   _delay_ms(200);
-   sbi(PORTE,6);
-   _delay_ms(200);
-   cbi(PORTE,6);
+  /* USER CODE END 1 */
 
-   orientation=LANDSCAPE;
+  /* MCU Configuration----------------------------------------------------------*/
 
-   UTFT_init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-   UTFT_initLCD(orientation);
-   setFrontColor(0, 0, 0);
-   setBackColor(255, 255, 255);
+  /* Configure the system clock */
+  SystemClock_Config();
 
-   uint16_t colors[] = {
-      VGA_BLACK,
-      VGA_WHITE,
-      VGA_RED,
-      VGA_GREEN,
-      VGA_BLUE,
-      VGA_SILVER,
-      VGA_GRAY,
-      VGA_MAROON,
-      VGA_YELLOW,
-      VGA_OLIVE,
-      VGA_LIME,
-      VGA_AQUA,
-      VGA_TEAL,
-      VGA_NAVY,
-      VGA_FUCHSIA,
-      VGA_PURPLE,
-   };
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
 
-//    uint16_t color = 0;
-//    while(1)
-//    {
-//       for (color = 0; color < sizeof(colors)/sizeof(colors[0]); color++)
-//       {
-//          fillScreen(colors[color]);
-//          _delay_ms(2000);
-//       }
-//    }
+  /* USER CODE BEGIN 2 */
 
-   setFont(SmallFont);
+  /* USER CODE END 2 */
 
-   // Clear the screen and draw the frame
-   clrScr();
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+    HAL_Delay(50);
 
-   // Draw crosshairs
-   setFrontColor(0, 0, 255);
-   setBackColor(0, 0, 0);
-   drawLine(239, 15, 239, 255);
-   drawLine(1, 135, 478, 135);
-   for (int i=9; i<470; i+=10)
-      drawLine(i, 133, i, 138);
-   for (int i=15; i<256; i+=10)
-      drawLine(237, i, 241, i);
+  /* USER CODE END WHILE */
 
-   // Draw sin-, cos- and tan-lines
-   setFrontColor(0,255,255);
-   print("Sin", 5, 15);
-   for (int i=1; i<478; i++)
-   {
-      drawPixel(i,135+(sin(((i*1.13)*3.14)/180)*95));
-   }
+  /* USER CODE BEGIN 3 */
 
-   setFrontColor(255,0,0);
-   print("Cos", 5, 27);
-   for (int i=1; i<478; i++)
-   {
-      drawPixel(i,135+(cos(((i*1.13)*3.14)/180)*95));
-   }
+  }
+  /* USER CODE END 3 */
 
-   setFrontColor(255,255,0);
-   print("Tan", 5, 39);
-   for (int i=1; i<478; i++)
-   {
-      drawPixel(i,135+(tan(((i*1.13)*3.14)/180)));
-
-   }
-
-   // setFrontColor(255,0, 255);
-   // print("Mansour's", 5, 51);
-   // for (int i=1; i<478; i++)
-   // {
-      // drawPixel(i,135+((((i-240)*(i-240))/pow(2.71,(i-240)))));
-
-   // }
-
-
-  _delay_ms(2000);
-  clrScr();
-
-  // Draw crosshairs
-   setFrontColor(0, 0, 255);
-   setBackColor(0, 0, 0);
-   drawLine(239, 15, 239, 255);
-   drawLine(1, 135, 478, 135);
-   for (int i=9; i<470; i+=10)
-      drawLine(i, 133, i, 138);
-   for (int i=15; i<256; i+=10)
-      drawLine(237, i, 241, i);
-
-   // Draw a moving sinewave
-  int buf[478];
-   int x=1;
-   int y=1;
-   for (int i=1; i<(478*20); i++)
-   {
-      x++;
-      if (x==479)
-         x=1;
-      if (i>479)
-      {
-         if ((x==239)||(buf[x-1]==135))
-            setFrontColor(0,0,255);
-         else
-            setFrontColor(0,0,0);
-
-         drawPixel(x,buf[x-1]);
-      }
-      setFrontColor(255,0,0);
-      y=135+(sin(((i*1.65)*3.14)/180)*(90-(i / 100)));
-      drawPixel(x,y);
-      buf[x-1]=y;
-   }
-
-  _delay_ms(2000);
-
-
-
-   // InitTouch(orientation);
-
-   // setFont(BigFont);
-
-   // clrScr();
-
-   // setFrontColor(0, 0, 0);
-   // setBackColor(255, 255, 255);
-   // fillScreen(VGA_WHITE);
-
-
-   // fillScreen(VGA_WHITE);
-   // print_P(text, 0, 0);
-
-   // setFont(SmallFont);
-   // setFrontColor(255, 0, 0);
-   // print_P(text2, 0, 100);
-
-   // setColor(VGA_BLUE);
-   // setFont(arial_17ptBitmaps);
-   // uint16_t number = 0;
-
-   // setFrontColor(0, 0, 0);
-
-   // while(1)
-   // {
-      // if (dataAvailable())
-      // {
-         // read();
-         // touch_x=getX();
-         // touch_y=getY();
-         // setCharXY(200, 136);
-         // fprintf_P(&lcd_str, PSTR("X  = %d"), touch_x);
-         // setCharXY(200, 166);
-         // fprintf_P(&lcd_str, PSTR("Y  = %d"), touch_y);
-      // }
-   // }
-
-
-   // while(1)
-   // {
-      // for(number = 0; number < 65535; number++)
-      // {
-         // setCharXY(200, 136);
-         // fprintf_P(&lcd_str, PSTR("0X%X"), number);
-      // }
-   // }
-
-
-
-   // uint16_t fontIndex = 0;
-   // for(fontIndex = 0; fontIndex < current_font.numchars; fontIndex++)
-   // {
-      // setCharXY(150, 120);
-      // putChar(current_font.offset+fontIndex);
-      // _delay_ms(500);
-   // }
-
-   while(1)
-   {
-   }
-
-   // setScrollArea(0,272);
-   // uint16_t scroll = 0;
-   // while(1)
-   // {
-      // for(scroll = 0; scroll < 272; scroll++)
-      // {
-         // _delay_ms(5);
-         // setScrollPosition(scroll);
-         // _delay_ms(5);
-      // }
-   // }
 }
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Configure the main internal regulator output voltage
+    */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /**Initializes the CPU, AHB and APB busses clocks
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Configure the Systick interrupt time
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler */
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+*/
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
